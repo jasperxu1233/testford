fc216fb560dca3c2f5a7e352f0cd229b3a47456c
Xu <jxu158@ford.com>
2022-08-03 19:14:29 -0400

    commit4

--- haojie/src/index.js
+++ haojie/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {useReduxContext} from "react-redux/es/hooks/useReduxContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
